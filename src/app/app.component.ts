2c4acdf6c5f5a0f56ad81134f94fb047ef6b42c0
Consu <david.consuegra.lujan@nttdata.com>
2023-04-08 21:54:36 +0200

    part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,26 @@ export class AppComponent {
     this.eventsinfo.getEvents().subscribe({
       next: (res: GetEvents[]) => {
         this.cards = res;
+        this.cards.sort((a, b) => {
+          const fechaA = new Date(+(a.endDate)).getTime();
+          const fechaB = new Date(+(b.endDate)).getTime();
+          return fechaA - fechaB;
+        });
+        this.cards.forEach((item) =>{
+          let numfecha: number = +item.startDate;
+          let fecha = new Date(numfecha);
+          let dia = fecha.getDate();
+          let mes = fecha.getMonth() + 1;
+          let anio = fecha.getFullYear();
+          item.startDate = `${dia}/${mes}/${anio}`;
+
+          numfecha = +item.endDate;
+          fecha = new Date(numfecha);
+          dia = fecha.getDate();
+          mes = fecha.getMonth() + 1;
+          anio = fecha.getFullYear();
+          item.endDate = `${dia}/${mes}/${anio}`;
+        });
       },
       error: () => {
       },
